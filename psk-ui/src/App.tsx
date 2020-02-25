39ee39562f2338aed720ebc7321c32eefe377305
Povilas Tamosauskas <povilast@hotmail.lt>
2020-02-25 21:20:35 +0200

    PSK-2: linted code

--- psk-ui/src/App.tsx
+++ psk-ui/src/App.tsx
@@ -1,48 +1,48 @@
-import React from "react";
-import { Layout, Menu, Icon } from "antd";
-import "./App.css";
-import { TEAM_NAME, CALENDAR_ITEM_NAME, USER_ITEM_NAME } from "./Constants";
-import CalendarView from "./pages/public/calendar-view";
-import { Route, Link, Switch } from "react-router-dom";
-import LandingPage from "./pages/public/landing/landing-page";
+import React from 'react';
+import { Layout, Menu, Icon } from 'antd';
+import './App.css';
+import { TEAM_NAME, CALENDAR_ITEM_NAME, USER_ITEM_NAME } from './Constants';
+import CalendarView from './pages/public/calendar-view';
+import { Route, Link, Switch } from 'react-router-dom';
+import LandingPage from './pages/public/landing/landing-page';
 
 const { Content, Footer, Sider } = Layout;
 
 const App: React.FunctionComponent<{}> = () => {
-  return (
-    <div className="App">
-      <Layout id="root-layout">
-        <Sider id="sider">
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-            <Menu.Item key="1">
-              <Link to="/profile">
-                <Icon type="user" />
-                <span className="nav-text">{USER_ITEM_NAME}</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/calendar">
-                <Icon type="calendar" />
-                <span className="nav-text">{CALENDAR_ITEM_NAME}</span>
-              </Link>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout id="main-content-layout">
-          <Content>
-            <Switch>
-              <Route exact path="/" component={LandingPage} />
-              <Route exact path="/calendar" component={CalendarView} />
-              <Route exact path="/calendar/:year/:month/:day" />
-              <Route exact path="/profile" />
-            </Switch>
-          </Content>
-          <Footer>Powered By: {TEAM_NAME}</Footer>
-        </Layout>
-      </Layout>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Layout id="root-layout">
+				<Sider id="sider">
+					<div className="logo" />
+					<Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+						<Menu.Item key="1">
+							<Link to="/profile">
+								<Icon type="user" />
+								<span className="nav-text">{USER_ITEM_NAME}</span>
+							</Link>
+						</Menu.Item>
+						<Menu.Item key="2">
+							<Link to="/calendar">
+								<Icon type="calendar" />
+								<span className="nav-text">{CALENDAR_ITEM_NAME}</span>
+							</Link>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout id="main-content-layout">
+					<Content>
+						<Switch>
+							<Route exact path="/" component={LandingPage} />
+							<Route exact path="/calendar" component={CalendarView} />
+							<Route exact path="/calendar/:year/:month/:day" />
+							<Route exact path="/profile" />
+						</Switch>
+					</Content>
+					<Footer>Powered By: {TEAM_NAME}</Footer>
+				</Layout>
+			</Layout>
+		</div>
+	);
 };
 
 export default App;
--- psk-ui/src/pages/public/calendar-view/index.tsx
+++ psk-ui/src/pages/public/calendar-view/index.tsx
@@ -1,79 +1,84 @@
-import React, { useState } from "react";
-import { Calendar, Badge } from "antd";
-import * as moment from "moment";
-import "./CalendarStyles.css";
+import React, { useState } from 'react';
+import { Calendar, Badge } from 'antd';
+import * as moment from 'moment';
+import './CalendarStyles.css';
 interface StatusType {
-  type: "warning" | "success" | "error" | "default" | "processing" | undefined;
+  type: 'warning' | 'success' | 'error' | 'default' | 'processing' | undefined;
   content: string;
 }
 
 const CalendarView: React.FunctionComponent<{}> = () => {
-  const handleOnSelect = (): void => {};
-  return (
-    <Calendar
-      dateCellRender={dateCellRender}
-      monthCellRender={monthCellRender}
-      onSelect={handleOnSelect}
-    />
-  );
+	const handleOnSelect = (): void => {};
+
+	return (
+		<Calendar
+			dateCellRender={dateCellRender}
+			monthCellRender={monthCellRender}
+			onSelect={handleOnSelect}
+		/>
+	);
 };
+
 function getListData(value: moment.Moment) {
-  let listData: StatusType[] = [];
-  switch (value.date()) {
-    case 8:
-      listData = [
-        { type: "warning", content: "This is warning event." },
-        { type: "success", content: "This is usual event." }
-      ];
-      break;
-    case 10:
-      listData = [
-        { type: "warning", content: "This is warning event." },
-        { type: "success", content: "This is usual event." },
-        { type: "error", content: "This is error event." }
-      ];
-      break;
-    case 15:
-      listData = [
-        { type: "warning", content: "This is warning event" },
-        { type: "success", content: "This is very long usual event。。...." },
-        { type: "error", content: "This is error event 1." },
-        { type: "error", content: "This is error event 2." },
-        { type: "error", content: "This is error event 3." },
-        { type: "error", content: "This is error event 4." }
-      ];
-      break;
-    default:
-  }
-  return listData || [];
+	let listData: StatusType[] = [];
+
+	switch (value.date()) {
+	case 8:
+		listData = [
+			{ type: 'warning', content: 'This is warning event.' },
+			{ type: 'success', content: 'This is usual event.' }
+		];
+		break;
+	case 10:
+		listData = [
+			{ type: 'warning', content: 'This is warning event.' },
+			{ type: 'success', content: 'This is usual event.' },
+			{ type: 'error', content: 'This is error event.' }
+		];
+		break;
+	case 15:
+		listData = [
+			{ type: 'warning', content: 'This is warning event' },
+			{ type: 'success', content: 'This is very long usual event。。....' },
+			{ type: 'error', content: 'This is error event 1.' },
+			{ type: 'error', content: 'This is error event 2.' },
+			{ type: 'error', content: 'This is error event 3.' },
+			{ type: 'error', content: 'This is error event 4.' }
+		];
+		break;
+	default:
+	}
+	return listData || [];
 }
 
 function dateCellRender(value: moment.Moment) {
-  const listData = getListData(value);
-  return (
-    <ul className="events">
-      {listData.map(item => (
-        <li key={item.content}>
-          <Badge status={item.type} text={item.content} />
-        </li>
-      ))}
-    </ul>
-  );
+	const listData = getListData(value);
+
+	return (
+		<ul className="events">
+			{listData.map(item => (
+				<li key={item.content}>
+					<Badge status={item.type} text={item.content} />
+				</li>
+			))}
+		</ul>
+	);
 }
 
 function getMonthData(value: moment.Moment) {
-  if (value.month() === 8) {
-    return 1394;
-  }
+	if (value.month() === 8) {
+		return 1394;
+	}
 }
 
 function monthCellRender(value: moment.Moment) {
-  const num = getMonthData(value);
-  return num ? (
-    <div className="notes-month">
-      <section>{num}</section>
-      <span>Backlog number</span>
-    </div>
-  ) : null;
+	const num = getMonthData(value);
+
+	return num ? (
+		<div className="notes-month">
+			<section>{num}</section>
+			<span>Backlog number</span>
+		</div>
+	) : null;
 }
 export default CalendarView;
