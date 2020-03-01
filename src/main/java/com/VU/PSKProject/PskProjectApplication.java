13d6e8f252b4e50b739a6f7031d77a8d3bf796b0
wheesko <41743424+wheesko@users.noreply.github.com>
2020-03-01 16:11:27 +0200

    PSK-1-2: Added initial backend app, local h2 database, test entity, controller, service, repository

---
+++ src/main/java/com/VU/PSKProject/PskProjectApplication.java
@@ -0,0 +1,13 @@
+package com.VU.PSKProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PskProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PskProjectApplication.class, args);
+	}
+
+}
