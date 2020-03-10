bb0ff3a8698d11f646e6e83f3c6935a5e626c426
Povilas Tamosauskas <povilast@hotmail.lt>
2020-03-10 16:38:45 +0200

    PSK-10: started work on worker, team entities

---
+++ src/main/java/com/VU/PSKProject/Entity/Team.java
@@ -0,0 +1,42 @@
+package com.VU.PSKProject.Entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    // private List<TeamGoals> teamGoals;
+
+    // team probably does not need a Manager reference
+    // you could just search for all team members, who are managers to get the object reference
+
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Worker> teamMembers = new ArrayList<>();
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
