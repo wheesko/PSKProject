1126d2469fc66e403a663837a588c3deea46525e
Lukaš Michnevič <lukas.michnevic@unity3d.com>
2020-04-06 20:43:14 +0300

    Added team CRS

---
+++ src/main/java/com/VU/PSKProject/Service/TeamService.java
@@ -0,0 +1,35 @@
+package com.VU.PSKProject.Service;
+
+import com.VU.PSKProject.Entity.Team;
+import com.VU.PSKProject.Repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeamService {
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public List<Team> getAllTeams(){
+        return teamRepository.findAll();
+    }
+
+    public void createTeam(Team team){
+        teamRepository.save(team);
+    }
+
+    public void updateTeam(Long id, Team team){
+        if(teamRepository.findById(id) != null) teamRepository.save(team);
+    }
+
+    public void deleteTeam(Long id){
+        teamRepository.deleteById(id);
+    }
+
+    public Optional<Team> getTeam(Long id){
+        return teamRepository.findById(id);
+    }
+}
