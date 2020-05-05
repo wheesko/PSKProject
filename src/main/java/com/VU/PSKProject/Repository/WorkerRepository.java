4092687e4f4359c027b1c3304ad1db9e246ae6ea
nastae <aurisgo1998@gmail.com>
2020-05-05 21:55:39 +0300

    Implement worker repository

--- src/main/java/com/VU/PSKProject/Repository/WorkerRepository.java
+++ src/main/java/com/VU/PSKProject/Repository/WorkerRepository.java
@@ -1,7 +1,15 @@
 package com.VU.PSKProject.Repository;
 
 import com.VU.PSKProject.Entity.Worker;
+import org.hibernate.jdbc.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
+import java.util.List;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>,
+        CrudRepository<Worker, Long> {
+
+    Worker findByManagedTeamId(Long managedTeamId);
+    List<Worker> findByWorkingTeamId(Long workingTeamId);
 }
