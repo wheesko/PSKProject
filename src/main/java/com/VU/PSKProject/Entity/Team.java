d7a398e40469527293f8355c03dd9c7a239a230c
nastae <aurisgo1998@gmail.com>
2020-05-05 19:21:09 +0300

    create worker entity

--- src/main/java/com/VU/PSKProject/Entity/Team.java
+++ src/main/java/com/VU/PSKProject/Entity/Team.java
@@ -1,39 +1,35 @@
 package com.VU.PSKProject.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Setter
+@Getter
 public class Team {
+
+    public Team() {
+
+    }
+
     @Id
     @GeneratedValue
     private Long id;
+
     private String name;
 
-    @OneToMany(
-            mappedBy = "team"
-    )
-    private List<Worker> teamMembers;
+    @OneToOne(mappedBy = "managedTeam")
+    @JsonIgnore
+    private Worker manager;
 
-    @OneToMany(
-            mappedBy = "team"
-    )
-    private List<TeamGoal> teamGoals;
+    @OneToMany(mappedBy = "workingTeam")
+    @JsonIgnore
+    private List<Worker> workers;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @OneToMany(mappedBy = "team")
+    private List<TeamGoal> goals;
 }
