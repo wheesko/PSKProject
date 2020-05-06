cb8a551a119615156cf98527f88333c5dddd4863
nastae <aurisgo1998@gmail.com>
2020-05-06 13:36:37 +0300

    Implement worker controller

--- src/main/java/com/VU/PSKProject/Controller/WorkerController.java
+++ src/main/java/com/VU/PSKProject/Controller/WorkerController.java
@@ -3,6 +3,7 @@ package com.VU.PSKProject.Controller;
 import com.VU.PSKProject.Entity.Worker;
 import com.VU.PSKProject.Service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +17,14 @@ public class WorkerController {
     private WorkerService workerService;
 
     @GetMapping
-    public List<Worker> getWorkers() {
-        return workerService.getAllWorkers();
+    public ResponseEntity<List<Worker>> getWorkers() {
+        return ResponseEntity.ok(workerService.getAllWorkers());
     }
 
     @GetMapping("{id}")
-    public Optional<Worker> getWorker(@PathVariable Long id) {
-        return workerService.getWorker(id);
+    public ResponseEntity<Worker> getWorker(@PathVariable Long id) {
+        Optional<Worker> worker = workerService.getWorker(id);
+        return worker.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
@@ -39,5 +41,16 @@ public class WorkerController {
     @DeleteMapping("{id}")
     public void deleteWorker(@PathVariable Long id) {
         workerService.deleteWorker(id);
+    }
+
+    @GetMapping("managed-teams/{id}")
+    public ResponseEntity<Worker> getWokerByManagedTeam(@PathVariable Long id) {
+        Optional<Worker> worker = workerService.findByManagedTeamId(id);
+        return worker.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("working-teams/{id}")
+    public ResponseEntity<List<Worker>> getWorkersByWorkingTeam(@PathVariable Long id) {
+        return ResponseEntity.ok(workerService.findByWorkingTeamId(id));
     }
 }
--- src/main/java/com/VU/PSKProject/Repository/WorkerRepository.java
+++ src/main/java/com/VU/PSKProject/Repository/WorkerRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkerRepository extends JpaRepository<Worker, Long>,
         CrudRepository<Worker, Long> {
 
-    Worker findByManagedTeamId(Long managedTeamId);
+    Optional<Worker> findByManagedTeamId(Long managedTeamId);
     List<Worker> findByWorkingTeamId(Long workingTeamId);
 }
