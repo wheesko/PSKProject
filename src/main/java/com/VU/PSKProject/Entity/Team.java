d4a472a686d55079d7e6d392a0f832d2cb8f4a82
cebukas <dijokas98@gmail.com>
2020-05-28 17:38:58 +0300

    fix

--- src/main/java/com/VU/PSKProject/Entity/Team.java
+++ src/main/java/com/VU/PSKProject/Entity/Team.java
@@ -39,7 +39,7 @@ public class Team {
     @JsonIgnore
     private List<Worker> workers;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "team")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private List<TeamGoal> goals;
     @Version
