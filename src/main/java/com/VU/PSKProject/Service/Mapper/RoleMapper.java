f5739349b4d754fa15b88c53b29e198285320b57
cebukas <dijokas98@gmail.com>
2020-05-31 21:11:57 +0300

    roleMapper fix

--- src/main/java/com/VU/PSKProject/Service/Mapper/RoleMapper.java
+++ src/main/java/com/VU/PSKProject/Service/Mapper/RoleMapper.java
@@ -1,10 +1,14 @@
 package com.VU.PSKProject.Service.Mapper;
 
 import com.VU.PSKProject.Entity.Role;
+import com.VU.PSKProject.Entity.RoleGoal;
 import com.VU.PSKProject.Service.Model.RoleDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RoleMapper {
@@ -12,6 +16,14 @@ public class RoleMapper {
     private ModelMapper modelMapper;
 
     public RoleDTO toDto(Role role){
-        return modelMapper.map(role, RoleDTO.class);
+        RoleDTO roleDTO = new RoleDTO();
+        roleDTO.setId(role.getId());
+        roleDTO.setName(role.getName());
+        List<Long> roleGoals = new ArrayList<>();
+        for (RoleGoal rg: role.getRoleGoals()) {
+            roleGoals.add(rg.getId());
+        }
+        roleDTO.setRoleGoals(roleGoals);
+        return roleDTO;
     }
 }
