2af9225dc48b329484d5ce1d7d645c57c6ef5c96
Povilas Tamosauskas <povilast@hotmail.lt>
2020-06-01 23:29:29 +0300

    exporting nf-7

--- src/main/psk-ui/src/pages/private/info-view/teams-by-topics/index.tsx
+++ src/main/psk-ui/src/pages/private/info-view/teams-by-topics/index.tsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
-import { Col, Row, Select, Spin, Table, Tooltip, Typography, Progress } from 'antd';
+import { Col, Row, Select, Spin, Table, Tooltip, Typography, Progress, Button } from 'antd';
 
 import { RootState } from '../../../../redux';
 import { useSelector } from 'react-redux';
 import notificationService, { NotificationType } from '../../../../service/notification-service';
 import topicService from '../../../../api/topic-service';
-import { LearningTopic } from "../../../../models/learningTopic";
+import { LearningTopic } from '../../../../models/learningTopic';
 import '../../team-members-view/worker-topic-table/WorkerTopicTableStyles.css';
-import { TeamResponse } from "../../../../api/model/team-response";
+import { TeamResponse } from '../../../../api/model/team-response';
 import teamService from '../../../../api/team-service';
-import { InfoCircleOutlined } from "@ant-design/icons/lib";
+import { InfoCircleOutlined, DownloadOutlined } from '@ant-design/icons/lib';
+import { CSVDownload, CSVLink } from "react-csv";
 
 const { Title } = Typography;
+
+interface CsvTeam {
+	id: number;
+	teamName: string;
+	employeeAmount: number;
+	learnedEmployeeAmount: number;
+	percentageOfLearnedEmployees: string;
+}
 
 const TeamsByTopics: React.FunctionComponent<{}> = () => {
 	const currentWorker = useSelector((state: RootState) => state.user);
@@ -21,6 +30,7 @@ const TeamsByTopics: React.FunctionComponent<{}> = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
 	const [allTeams, setAllTeams] = useState<TeamResponse[]>([]);
+	const [csvData, setCsvData] = useState<CsvTeam[]>([]);
 
 
 	function getWorkersTopicsByManager() {
@@ -53,6 +63,30 @@ const TeamsByTopics: React.FunctionComponent<{}> = () => {
 		});
 	}, [currentWorker.managedTeamId]);
 
+	useEffect(() => {
+		setCsvData(selectedTopics.length === 0
+			? allTeams.map(team => {
+				return {
+					id: team.id,
+					teamName: team.name,
+					employeeAmount: team.workers.length,
+					learnedEmployeeAmount: 0,
+					percentageOfLearnedEmployees: '0%'
+				};
+			})
+			: allTeams.map(team => {
+				return {
+					id: team.id,
+					teamName: team.name,
+					employeeAmount: team.workers.length,
+					learnedEmployeeAmount: selectedTopics.length === 0 ? 0 : getLearnedCountInTeam(team.id, team),
+					percentageOfLearnedEmployees: selectedTopics.length === 0 ? '0%' : getLearnedCountInTeam(team.id, team) > 0
+						? Math.trunc((getLearnedCountInTeam(team.id, team) / team.workers.length) * 100) + '%'
+						: 0 + '%'
+				};
+			}));
+	}, [selectedTopics.length]);
+
 	function loadTopics(): Promise<LearningTopic[]> {
 		return topicService.getAllTopics();
 	}
@@ -66,22 +100,24 @@ const TeamsByTopics: React.FunctionComponent<{}> = () => {
 		setSelectedTopics(selectedTopics.filter(topic => topic !== value));
 	};
 
-	function renderLearnedPercentage(id: string, team: TeamResponse): React.ReactNode {
-		let learningPercentage = getLearnedCountInTeam(id, team) > 0
+	function renderLearnedPercentage(id: number, team: TeamResponse): React.ReactNode {
+		const learningPercentage = getLearnedCountInTeam(id, team) > 0
 			? Math.trunc((getLearnedCountInTeam(id, team) / team.workers.length) * 100)
 			: 0;
-		return selectedTopics.length === 0 ? <Progress percent={0}/> : <Progress percent={learningPercentage}/>
+
+		return selectedTopics.length === 0 ? <Progress percent={0}/> : <Progress percent={learningPercentage}/>;
 	}
 
-	function getLearnedCountInTeam(id: string, team: TeamResponse) {
+	function getLearnedCountInTeam(id: number, team: TeamResponse) {
 		return team.workers.filter((worker) => {
-			let workersLearnedTopicNames = worker.learningDays
+			const workersLearnedTopicNames = worker.learningDays
 				.filter(learningDay => learningDay.learned)
 				.map(learningDay => {
-					return learningDay.topic.name
+					return learningDay.topic.name;
 				});
+
 			return selectedTopics.every(selectedTopic => workersLearnedTopicNames.includes(selectedTopic));
-		}).length
+		}).length;
 	}
 
 	const columns = [
@@ -89,15 +125,15 @@ const TeamsByTopics: React.FunctionComponent<{}> = () => {
 			title: 'Team name',
 			dataIndex: 'id',
 			key: 'id',
-			render: (id: string, team: TeamResponse, index: number) => {
-				return <Typography.Text>{team.name}</Typography.Text>
+			render: (id: number, team: TeamResponse, index: number) => {
+				return <Typography.Text>{team.name}</Typography.Text>;
 			},
 		},
 		{
 			title: 'Employee amount',
 			dataIndex: 'id',
 			key: 'employeeCount',
-			render: (id: string, team: TeamResponse, index: number) => {
+			render: (id: number, team: TeamResponse, index: number) => {
 				return team.workers.length;
 			}
 		},
@@ -111,30 +147,43 @@ const TeamsByTopics: React.FunctionComponent<{}> = () => {
 				</>,
 			dataIndex: 'id',
 			key: 'learnedCount',
-			render: (id: string, team: TeamResponse, index: number) => {
+			render: (id: number, team: TeamResponse, index: number) => {
 				return selectedTopics.length === 0 ? '' :
 					team.workers.filter((worker) => {
 						return worker.learningDays.some(learningDay => {
 							return learningDay.learned ? selectedTopics?.includes(learningDay.topic.name) : false;
-						})
-					}).length
+						});
+					}).length;
 			}
 		},
 		{
 			title: 'Percentage of people learned selected topics',
 			dataIndex: 'id',
 			key: 'learnedPercentage',
-			render: (id: string, team: TeamResponse) =>
+			render: (id: number, team: TeamResponse) =>
 				renderLearnedPercentage(id, team)
 		}
 	];
 
 	return <>
 		<Spin spinning={isLoading} size="large">
-			<Row justify={"start"}>
+			<Row justify={'space-between'}>
 				<Typography.Title level={2}>Filter teams by learning topics</Typography.Title>
+				<CSVLink
+					data={csvData}
+					filename={`teamsByLearningTopics[${selectedTopics.join('-')}].csv`}
+
+					className="btn btn-primary"
+					target="_blank"
+				>
+					<Button disabled={selectedTopics.length === 0}
+							icon={<DownloadOutlined/>}>Export to
+						CSV</Button>
+				</CSVLink>
 			</Row>
-			<Row justify={"start"} className={"topic-row"}>
+			<Row>
+			</Row>
+			<Row justify={'start'} className={'topic-row'}>
 				<Col span={24}>
 					<Select mode="tags"
 							onSelect={onTopicSelected}
